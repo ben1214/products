ed054a40ccf607c019c3ecfe864a8f3f85b1b7ec
ben1214 <aaron690322@icloud.com>
2023-08-03 20:49:56 +0800

    新增商品及價格

---
+++ product.py
@@ -0,0 +1,13 @@
+products = []
+while True:
+	name = input('輸入商品名稱:')
+	if name == 'q':
+		break
+	price = input('輸入價格:')
+	products.append([name,price])
+print(products)
+
+print(products[0][0])
+
+
+
