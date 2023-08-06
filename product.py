4b138894c7ea6c622e5a08b6b35614e2a75cbfa1
ben1214 <aaron690322@icloud.com>
2023-08-06 19:25:20 +0800

    增加數量及小計

--- product.py
+++ product.py
@@ -2,10 +2,10 @@
 products = []
 with open('products.csv', 'r', encoding='big5') as f:
 	for line in f:
-		if '商品,價格' in line:
+		if '商品,價格,數量,小計金額' in line:
 			continue
-		name, price = line.strip().split(',')
-		products.append([name,price])
+		name, price, quality, total = line.strip().split(',')
+		products.append([name,price,quality,total])
 print(products)
 
 # 請使用者輸入
@@ -15,16 +15,18 @@ while True:
 		break
 	price = input('輸入價格:')
 	price = int(price)
-	products.append([name,price])
+	quality = input('輸入數量:')
+	quality = int(quality)
+	total = price * quality
+	products.append([name,price,quality,total])
 print(products)
 
 # 印出所有購買紀錄
 for p in products:
-	print(p[0],'的價格是',p[1])
-
+	print(p[0],'的價格是',p[1],'數量',p[2],'小計金額',p[3])
 #寫入檔案	
 with open('products.csv','w' , encoding='big5') as f:
-	f.write('商品,價格\n')
+	f.write('商品,價格,數量,小計金額\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
 
