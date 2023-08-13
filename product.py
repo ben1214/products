bc742ae6177df31a1d4ca1f99058544a90119ab4
ben1214 <aaron690322@icloud.com>
2023-08-13 15:58:58 +0800

    亂改function

--- product.py
+++ product.py
@@ -1,47 +1,55 @@
+
 import os
 products = []
-if os.path.isfile('product.csv'):
-	print('yes')
-	with open('products.csv', 'r', encoding='big5') as f:
+
+# 讀取檔案
+def read_file(filename):
+	
+	with open(filename, 'r', encoding='big5') as f:
 		for line in f:
 			if '商品,價格,數量,小計金額' in line:
 				continue
 			name, price, quality, total = line.strip().split(',')
 			products.append([name,price,quality,total])
-	print(products)
-
-else:
-	print('no')
-
-
-
-# 讀取檔案
+	return products
 
 # 請使用者輸入
-while True:
-	name = input('輸入商品名稱:')
-	if name == 'q':
-		break
-	price = input('輸入價格:')
-	price = int(price)
-	quality = input('輸入數量:')
-	quality = int(quality)
-	total = price * quality
-	products.append([name,price,quality,total])
-print(products)
+def user_input(products):
+	while True:
+		name = input('輸入商品名稱:')
+		if name == 'q':
+			break
+		price = input('輸入價格:')
+		price = int(price)
+		quality = input('輸入數量:')
+		quality = int(quality)
+		total = price * quality
+		products.append([name,price,quality,total])
+	print(products)
+	return products
 
 # 印出所有購買紀錄
-for p in products:
-	print(p[0],'的價格是',p[1],'數量',p[2],'小計金額',p[3])
-
-#寫入檔案	
-with open('products.csv','w' , encoding='big5') as f:
-	f.write('商品,價格,數量,小計金額\n')
+def print_products(products):
 	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+		print(p[0],'的價格是',p[1],'數量',p[2],'小計金額',p[3])
 
+#寫入檔案
+def write_flie(filename,products):
+	with open(filename,'w' , encoding='big5') as f:
+		f.write('商品,價格,數量,小計金額\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + ',' + str(p[2]) + ',' + str(p[3]) + '\n')
+def main():
 
-
-
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('yes')
+		products = read_file(filename)
+	else:
+		print('no')
+	products = user_input(products)
+	print_products(products)
+	write_flie('products.csv', products)
+main()
 
 
